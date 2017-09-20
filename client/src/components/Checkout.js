1530f8ff4cd02f2b67cf712397d134a3d5fae873
Alex Ha <alexh.90@live.com>
2017-09-20 02:20:05 -0700

    Add calculation table for checkout page. Addons buggy. Turn on/off together

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Table } from 'react-bootstrap';
 
-//to-do: add room number after selection?
-// bug: all items change together
+//to-do: integrate stripe
+// bug: all addons change together
 // checkbox reference: https://stackoverflow.com/questions/32923255/react-checkbox-doesnt-toggle
 // price sheet:
 //      frugal traveler: 75,
@@ -19,6 +19,11 @@ class Checkout extends Component {
         super(props);
         this.state = {
             numAdults: 2,
+            numNights: 6,
+            roomCost: 150,
+            cleaningCost: 12,
+            occupancyTax: 0.0525,
+            tourismTax: 0.03125,
             roomSelection: 'executiveSuite',
             selectedDates: '',
             carePackage: false,
@@ -27,7 +32,7 @@ class Checkout extends Component {
             breakfast: false,
         };
         this.handleChange = this.handleChange.bind(this);
-
+        this.addonCost = this.addonCost.bind(this);
     }
 
     handleChange(event) {
@@ -37,6 +42,34 @@ class Checkout extends Component {
             shuttleRide: !this.state.shuttleRide,
             breakfast: !this.state.breakfast,
         });
+        /* use this to inject addons into table and update total
+        return (
+            <li>{alert('hello')}</li>
+        );
+        */
+
+    }
+
+    addonCost() {
+        // broken. all items turn on/off together. Tax is not added for these.
+        let carePackageCost = 5;
+        let lateCheckoutCost = 10;
+        let shuttleRideCost = 25;
+        let breakfastCost = 10;
+
+        if (!this.state.carePackage) {
+            carePackageCost = 0;
+        }
+        if (!this.state.lateCheckout) {
+            lateCheckoutCost = 0;
+        }
+        if (!this.state.shuttleRide) {
+            shuttleRideCost = 0;
+        }
+        if (!this.state.breakfast) {
+            breakfastCost = 0;
+        }
+        return carePackageCost + lateCheckoutCost + shuttleRideCost + breakfastCost;
     }
 
     render() {
@@ -48,34 +81,86 @@ class Checkout extends Component {
                     and {this.state.numAdults} adults. We hope you enjoy your stay!
                     Please consider the following add-ons before checking out:
                 </h1>
-                <div className="optionalAddons">
-                    <label>
-                        <input type="checkbox" onChange={this.handleChange} value={this.state.carePackage} />
-                        &nbsp; (+$5.00) Carepackage (toothbrush, toothpaste, water, gum)
-                    </label>
-                    <br />
-                    <label>
-                        <input type="checkbox" onChange={this.handleChange} value={this.state.lateCheckout} />
-                        &nbsp; (+$10.00) Late checkout (2:00pm PST)
-                    </label>
-                    <br />
-                    <label>
-                        <input type="checkbox" onChange={this.handleChange} value={this.state.shuttleRide} />
-                        &nbsp; (+$25.00) San Francisco Airport shuttle
-                    </label>
-                    <br />
-                    <label>
-                        <input type="checkbox" onChange={this.handleChange} value={this.state.breakfast} />
-                        &nbsp; (+10.00/guest) Continential Breakfast
-                    </label>
-                </div>
 
+                Your stay:
+                <Table striped bordered condensed>
+                    <thead>
+                    <tr>
+                        <th>Item/Description</th>
+                        <th>Cost</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Room base charge (${this.state.roomCost.toFixed(2)} x {this.state.numNights} nights)</td>
+                        <td>${(this.state.roomCost * this.state.numNights).toFixed(2)}</td>
+                    </tr>
+                    <tr>
+                        <td>Cleaning fee (${this.state.cleaningCost.toFixed(2)} x {this.state.numNights} nights)</td>
+                        <td>${(this.state.cleaningCost * this.state.numNights).toFixed(2)}</td>
+                    </tr>
+                    <tr>
+                        <td>S.F. occupancy tax ({(this.state.occupancyTax * 100).toFixed(3)}%</td>
+                        <td>${(((this.state.roomCost + this.state.cleaningCost) * this.state.numNights) * this.state.occupancyTax).toFixed(2)}</td>
+                    </tr>
+                    <tr>
+                        <td>S.F. tourism tax ({(this.state.tourismTax * 100).toFixed(3)}%)</td>
+                        <td>${(((this.state.roomCost + this.state.cleaningCost) * this.state.numNights) * this.state.tourismTax).toFixed(2)}</td>
+                    </tr>
+                    <tr>
+                        <td> </td>
+                        <td> </td>
+                    </tr>
+                    <tr>
+                        <td>Optional addons:</td>
+                        <td> </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>
+                                <input type="checkbox" onChange={this.handleChange} value={this.state.carePackage} />
+                                &nbsp;Carepackage (toothbrush, toothpaste, water, gum)
+                            </label>
+                        </td>
+                        <td>{this.addonCost()}</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>
+                                <input type="checkbox" onChange={this.handleChange} value={this.state.lateCheckout} />
+                                &nbsp;Late checkout (2:00pm PST)
+                            </label>
+                        </td>
+                        <td>{this.addonCost()}</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>
+                                <input type="checkbox" onChange={this.handleChange} value={this.state.shuttleRide} />
+                                &nbsp;San Francisco Airport shuttle
+                            </label>
+                        </td>
+                        <td>{this.addonCost()}</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>
+                                <input type="checkbox" onChange={this.handleChange} value={this.state.breakfast} />
+                                &nbsp;Continential Breakfast (charged per guest)
+                            </label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>TOTAL</td>
+                        <td>${(((this.state.roomCost + this.state.cleaningCost) * this.state.numNights)
+                            + (((this.state.roomCost + this.state.cleaningCost) * this.state.numNights) * this.state.occupancyTax)
+                            + (((this.state.roomCost + this.state.cleaningCost) * this.state.numNights) * this.state.tourismTax)
+                            + this.addonCost()).toFixed(2)}
+                        </td>
+                    </tr>
+                    </tbody>
+                </Table>
 
-                Room cost: $99.99 x 6 nights
-                Cleaning fee: $15.00 x 6 nights
-                S.F. occupancy tax (14%): $42.00
-                S.F. tourism tax (1.5%): $35.23
-                <h3>Total cost of your stay: $765.23</h3>
                 <h2>Enter credit card information</h2>
                 <Button bsStyle="primary" href="/confirmation">Pay Later (cancel by 12/24/2017)</Button>
                 <Button bsStyle="success" href="/confirmation">Pay Now</Button>

d36d4c2cacc0772b575daf5a0af641e5f772deaf
Alex Ha <alexh.90@live.com>
2017-10-06 21:59:07 -0700

    fixed path index.js. Heroku deploy 11

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 //===============================================================================================//
 
 // mongoose DB for model schema

dbed6490754b4a3ca6ab386b334877912367509a
Alex Ha <alexh.90@live.com>
2017-10-07 01:04:34 -0700

    get route chnage. Heroku deploy 22

--- index.js
+++ index.js
@@ -56,24 +56,22 @@ app.get('/api/test', (req, res) => {
 // =================================================================================================
 
 if (process.env.NODE_ENV === 'production') {
-    //app.use(express.static('client/build'));
 
-    app.use('/alexha-90/HotelNoMas.git/', express.static(path.join(__dirname, 'alexha-90/HotelNoMas.git/')));
-
-    //app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
     // look inside react client side for files @ /client/build
     //app.use(express.static('client/build'));
-    /*
 
+    /*
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname+'/client/build/index.html'));
     });
     */
+
     // if Express is unable to locate a route, serve index.html
-    //app.get('*', (req, res) => {
-    //    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    //});
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 

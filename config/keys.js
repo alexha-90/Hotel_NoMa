0685b4c52821636ef02ced497c7282cfaec8bdd2
Alex Ha <alexh.90@live.com>
2017-10-07 00:22:21 -0700

    removed prod.js key route from last commit. Causing errors again...

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,13 @@
 if (process.env.NODE_ENV === "production") {
     console.log('******** in production environment ********');
-    module.exports = require("./prod.js");
+    // not used... causes an error during deployment. module.exports = require("./prod.js");
+    module.exports = {
+        mongoURI: process.env.MONGO_URI,
+        stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+        stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+        emailPassword: process.env.EMAIL_PASSWORD
+    };
+
 } else {
     console.log('******** in development environment ********');
     module.exports = require("./dev.js");
